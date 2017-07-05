c105576b98794418c4c4100bb33ae67c9fa7bd75
marcfortin2 <marc.fortin@logipromedic.com>
2017-07-05 12:41:47 -0400

    Update db.php

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -12,3 +12,32 @@ return array(
 		),
 	),
 );
+/*return array(
+        'development' => array(
+		'connection'  => array(
+                        'dsn'        => 'firebird:dbname=127.0.0.1:d:\php\tbweb\tbweb.ib',
+			'username'   => 'SYSDBA',
+			'password'   => 'masterkey',
+		),
+                'profiling'    => true,
+		'enable_cache'   => false,
+	),
+	'default' => array(
+		'connection'  => array(
+                        'dsn'        => 'firebird:dbname=127.0.0.1:d:\php\tbweb\tbweb.ib',
+			'username'   => 'SYSDBA',
+			'password'   => 'masterkey',
+		),
+                'profiling'    => true,
+		'enable_cache'   => false,
+	),
+        'test' => array(
+		'connection'  => array(
+                        'dsn'        => 'firebird:dbname=127.0.0.1:d:\php\tbweb\tbweb.ib',
+			'username'   => 'SYSDBA',
+			'password'   => 'masterkey',
+		),
+                'profiling'    => true,
+		'enable_cache'   => false,
+	),
+);*/
